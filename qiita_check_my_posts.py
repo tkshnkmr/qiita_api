920dedd3be93259ae55dd8f65ce44f02f416f048
tkshnkmr <tkshnkmr@users.noreply.github.com>
2017-09-22 11:52:18 +0100

    Update qiita_check_my_posts.py

--- qiita_check_my_posts.py
+++ qiita_check_my_posts.py
@@ -3,27 +3,27 @@
 import json, requests, sys, getpass
 
 def get_token():
-    print "\nユーザー名を入力して下さい"
+    print "\nEnter User name"
     user_id = sys.stdin.readline()
     user_id = user_id.rstrip('\n')
 
-    print '\nパスワードを入力して下さい'
+    print '\nEnter Password'
     password = getpass.getpass()
 
     post_token_data = {'url_name': user_id, 'password': password}
     token_request = requests.post(url='https://qiita.com/api/v1/auth', data=post_token_data)
 
     if "token" in token_request.json:
-        print "認証成功"
+        print "Verify"
         return token_request.json
     else:
-        print "User id, または Password が違います．"
+        print "Wrong User id or Password"
         exit()
 
 #--------------------------------------------
 # Cheak my dailyreport
 #--------------------------------------------
-print "\n自分が投稿した記事を閲覧します．\nQiita Teamの記事を閲覧する場合，Team名を入力して下さい\nそうでない場合，Enterを押して下さい"
+print "\nBrowse your post \n If you want to browse Qiita Team, Enter Team name, \n otherwise, press Enter"
 
 team_id = sys.stdin.readline()
 team_id = team_id.rstrip('\n')
